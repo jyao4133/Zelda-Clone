114266c12a84f919d84bddba5ac989de265a855d
sbha375 <48309168+sbha375@users.noreply.github.com>
2019-04-19 22:49:54 +1200

    adding map for the 3rd room

--- src/com/level/level3.java
+++ src/com/level/level3.java
@@ -4,23 +4,30 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 import com.control.Game;
+import com.control.ImageRender;
 
 public class level3 {
 
     Game game;
+    private BufferedImage bedroom;
+    ImageRender loader = new ImageRender();
 
-
+    public level3 () {
+    	bedroom = loader.loadImage("Bedroom1.png");
+    }
     private void tick() {
     }
 
 
     public void render(Graphics g){
-        g.setColor(Color.red);
-        g.fillRect(0, 150, 1040, 990);
-
+      //  g.setColor(Color.red);
+      // g.fillRect(0, 150, 1040, 990);
         //Health Bar area
         g.setColor(Color.black);
         g.fillRect(0, 0,1040, 170);
+        
+    	g.drawImage(bedroom, 0, 170, null);
+
 
     }
 
